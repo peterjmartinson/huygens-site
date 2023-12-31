5fdb74ebf7f91b0b2f5d1c3f7da8ce8670bf6746
Jay Schwane <jrschwane@uwalumni.com>
2023-12-30 19:09:17 -0600

    fix: repair linting errors

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export function Footer () {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Powered by{' '}
+        <span className={styles.logo}>
+          <Image
+            alt="Vercel Logo"
+            height={16}
+            src="/vercel.svg"
+            width={72}
+          />
+        </span>
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
