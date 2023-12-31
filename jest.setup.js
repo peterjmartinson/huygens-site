e2516c66136f2fe819c29ada3c23598dc52c3ed5
Jay Schwane <jrschwane@uwalumni.com>
2023-12-30 19:16:38 -0600

    test: globally mock canvas getContext

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom'
+
+// globally mock the `getContext` method during tests
+HTMLCanvasElement.prototype.getContext = () => ({
+  // return whatever getContext has to return
+  fillRect: jest.fn(),
+  fillStyle: null
+})
