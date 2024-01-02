d3d8e283fa04edfb1e8fbcae6ee5a612cfaf6593
Jay Schwane <jrschwane@uwalumni.com>
2024-01-02 14:59:03 -0600

    fix: pass config object to draw methods
    Currently only need this for scale (aka: pixel ratio).
    We may use this config object for additional props
    in the future.

--- src/components/Canvas/CanvasExample.jsx
+++ src/components/Canvas/CanvasExample.jsx
@@ -5,12 +5,17 @@ import { CanvasBuilder } from './CanvasBuilder'
 /**
  * @type {import('./CanvasBuilder').DrawFactory}
  */
-function drawExample (canvas) {
+function drawExample (canvas, { scale }) {
   return {
     draw () {
       const ctx = canvas.getContext('2d')
       ctx.fillStyle = 'green'
-      ctx.fillRect(0, 0, 100, 100)
+      ctx.fillRect(
+        0,
+        0,
+        100 * scale,
+        100 * scale
+      )
     }
   }
 }
