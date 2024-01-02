f67100ca70b576c0c1ec86a826e15acf986e7865
Jay Schwane <jrschwane@uwalumni.com>
2024-01-02 12:13:30 -0600

    chore: use jest-canvas-mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,12 +1,4 @@
 import '@testing-library/jest-dom'
 
-// globally mock the `getContext` method during tests
-HTMLCanvasElement.prototype.getContext = () => ({
-  // return whatever getContext has to return
-  arc: jest.fn(),
-  fill: jest.fn(),
-  beginPath: jest.fn(),
-  clearRect: jest.fn(),
-  fillRect: jest.fn(),
-  fillStyle: null
-})
+// https://www.npmjs.com/package/jest-canvas-mock
+import 'jest-canvas-mock'
