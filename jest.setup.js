a953a411801687d32545ee617972afb18271321a
Jay Schwane <jrschwane@uwalumni.com>
2024-01-02 00:50:48 -0600

    test: globally mock more canvas ctx methods

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,10 @@ import '@testing-library/jest-dom'
 // globally mock the `getContext` method during tests
 HTMLCanvasElement.prototype.getContext = () => ({
   // return whatever getContext has to return
+  arc: jest.fn(),
+  fill: jest.fn(),
+  beginPath: jest.fn(),
+  clearRect: jest.fn(),
   fillRect: jest.fn(),
   fillStyle: null
 })
