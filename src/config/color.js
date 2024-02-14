c47ec6a12a854233eafe98998ce21f1a8eb5c31f
Peter Martinson <pmartinson@dkp.com>
2024-02-14 11:06:37 -0500

    Add BLACK to the colors

--- src/config/color.js
+++ src/config/color.js
@@ -3,9 +3,11 @@
 export const RED = 'rgb(212, 42, 32)'
 export const YELLOW = 'rgb(250, 194, 43)'
 export const BLUE = 'rgb(14, 99, 142)'
+export const BLACK = 'rgb(0, 0, 0)'
 
 export const Color = Object.freeze({
   RED,
   YELLOW,
-  BLUE
+  BLUE,
+  BLACK
 })
